4c914a2339d97fa5ea1dd30c7399b3f19b8a7543
Miguel A. Pardo Alvarez <miguelpardoalvare50@gmail.com>
2024-11-19 16:17:15 -0500

    Configuracion del seguridad CORS

--- src/main/java/apifestivos/apifestivos/aplicacion/seguridad/ConfiguracionSeguridad.java
+++ src/main/java/apifestivos/apifestivos/aplicacion/seguridad/ConfiguracionSeguridad.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,12 +30,30 @@ public class ConfiguracionSeguridad {
     // Configurando Seguridad Http
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception {
-        return https.csrf(csrf -> csrf.disable()) // Deshabilita la protección CSRF
+        return https.cors(cors -> cors.configurationSource(corsConfigurationSource())) // Habilita CORS
+                .csrf(csrf -> csrf.disable()) // Deshabilita la protección CSRF
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/usuarios/login/**").permitAll()
                         .anyRequest().authenticated()) // endpoints sin tokens
                 .addFilterAfter(filtro, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200"); // URL de frontend
+        config.addAllowedHeader("Authorization"); // Permite encabezados personalizados como Authorization
+        config.addAllowedHeader("Content-Type"); // Permite encabezados estándar
+        config.addAllowedMethod("GET"); // Métodos HTTP permitidos
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.setAllowCredentials(true); // Permite cookies/sesiones si es necesario
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Aplica a todas las rutas
+        return source;
     }
 
 }
